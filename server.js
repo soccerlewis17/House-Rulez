b488b40b99631cb0265c042704158179840704e2
soccerlewis17 <soccerlewis17@gmail.com>
2023-02-03 10:03:13 -0500

    prep for deployment

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ import './config/database.js'
 // Require controllers here
 const app = express();
 
+app.set('view engine', 'ejs');
+
 // console.log(assetsRouter)
 // add in when the app is ready to be deployed
 // app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
@@ -39,9 +41,13 @@ app.use('/api/users', userRoutes);
 app.use('/api/games', gameRoutes);
 app.use('/api', commentRoutes);
 app.use('/api', likeRoutes);
-// "catch all" route
+
+app.use(express.static(path.join(__dirname, "dist")));
+
+import manifest from './dist/manifest.json' assert {type: "json"};
+
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.render(path.join(__dirname, 'dist', 'index.ejs'), {manifest});
 });
 
 const { PORT } = process.env;
